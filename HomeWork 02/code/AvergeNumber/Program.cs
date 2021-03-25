7215d1005811cd3c841a2e2a049723127a2c63ac
fgjorgevski <74385707+fgjorgevski@users.noreply.github.com>
2021-03-26 00:04:57 +0100

    Homework added

---
+++ HomeWork 02/code/AvergeNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace AvergeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter four numbers to calculate the averge of them: \n");
+            Console.Write("First Number: ");
+            string firstNumber = Console.ReadLine();
+            Console.Write("Second Number: ");
+            string secondNumber = Console.ReadLine();
+            Console.Write("Third Number: ");
+
+            string thirdNumber = Console.ReadLine();
+            Console.Write("Fourth Number: ");
+            string fourthNumber = Console.ReadLine();
+
+            int[] arrOfNumb = { int.Parse(firstNumber), int.Parse(secondNumber), int.Parse(thirdNumber), int.Parse(fourthNumber) };
+            int num;
+            bool numCheck = int.TryParse(firstNumber, out num);
+            bool numCheck2 = int.TryParse(secondNumber, out num);
+            bool numCheck3 = int.TryParse(thirdNumber, out num);
+            bool numCheck4 = int.TryParse(fourthNumber, out num);
+
+            if (numCheck && numCheck2 && numCheck3 && numCheck4)
+            {
+
+            int sum = 0;
+            foreach(int item in arrOfNumb)
+            {
+                sum += item;
+            }
+            Console.WriteLine("The sum of this numbers is " + sum);
+            int result = sum / arrOfNumb.Length;
+            Console.WriteLine("The average of "+ firstNumber +", "+ secondNumber+", "+ thirdNumber + ", " + fourthNumber +" is "+ result);
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter number");
+            }
+
+
+
+        }
+    }
+}
---
+++ HomeWork 02/code/SwapNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace SwapNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Input 2 numbes and swap the values");
+            Console.Write("First number: ");
+            string firstNumber = Console.ReadLine();
+            Console.Write("Second number: ");
+            string secondNumber = Console.ReadLine();
+            int.Parse(firstNumber);
+            int.Parse(secondNumber);
+            Console.WriteLine("Numbers before swapping");
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+
+
+            Console.WriteLine("Numbers after swapping");
+            (firstNumber, secondNumber) = (secondNumber, firstNumber);
+            Console.WriteLine(firstNumber);
+            Console.WriteLine(secondNumber);
+
+
+            
+            
+
+
+        }
+    }
+}
