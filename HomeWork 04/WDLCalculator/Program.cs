a1d9c49f2b14d4e129365ed811d6292f18d029ca
fgjorgevski <74385707+fgjorgevski@users.noreply.github.com>
2021-04-05 18:05:48 +0200

    HomeWork 04 added

---
+++ HomeWork 04/WDLCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace WDLCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int checkedWins;
+            int checkedDraws;
+            int checkedLosses;
+
+
+
+            Console.Write("Please enter wins: ");
+            bool winsInput = int.TryParse(Console.ReadLine(), out checkedWins);
+            Console.Write("Enter draws: ");
+            bool drawsInput = int.TryParse(Console.ReadLine(), out checkedDraws);
+            Console.Write("Enter losses: ");
+            bool lossesInput = int.TryParse(Console.ReadLine(), out checkedLosses);
+
+            int WDLcalculatorMethod = WDLcalculator(checkedWins, checkedDraws, checkedLosses);
+
+            if (winsInput && drawsInput && lossesInput)
+            {
+                string methodString = $"You have {WDLcalculatorMethod} points";
+                Console.WriteLine(methodString);
+            }
+            else
+            {
+                Console.WriteLine("Please enter number of wins, draws and losses");
+            }
+
+            
+        }
+
+        static public int WDLcalculator(int wins, int draws, int losses )
+        {
+            return wins * 3 + draws * 1 + losses * 0;
+
+        }
+    }
+}
---
+++ HomeWork 04/yearsToDays/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace yearsToDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello if you want to see how many days old you are, please enter your age in years");
+            Console.Write("Age: ");
+            string inputFromUser = Console.ReadLine();
+            
+            int checkedInputFromUser;
+            bool inputCheck = int.TryParse(inputFromUser,out checkedInputFromUser);
+
+            int yearsMethod = YearsToDays(checkedInputFromUser);
+            if (inputCheck)
+            {
+
+                string daysBorn = $"You are {yearsMethod} days old";
+                Console.Write(daysBorn);
+            }
+            else
+            {
+                Console.WriteLine("Please enter your birth year.");
+            }
+        }
+
+        static public int YearsToDays(int age)
+        {
+
+            return age * 365;
+        }
+
+    }
+}
